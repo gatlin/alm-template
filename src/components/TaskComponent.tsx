7baf8c1f36431505f737a85e2461f20d5d5e6b5e
Gatlin Johnson <gatlin@niltag.net>
2017-11-15 13:36:31 -0600

    cleanup

--- src/components/TaskComponent.tsx
+++ src/components/TaskComponent.tsx
@@ -7,59 +7,28 @@ import ViewingTask from './ViewingTask';
 const TaskComponent = props => {
     const { uid, description, completed, edit, update } = props;
     const content = props.editing
-                  ? EditingTask({ uid, description, update })
-          : ViewingTask({ uid, description, completed, edit });
+        ? EditingTask({ uid, description, update })
+        : ViewingTask({ uid, description, completed, edit });
 
     return (
-        <li id={'task-'+uid} className='task'>
-          <input
-            type='checkbox' className='toggle'
-            id={'check-task-'+uid}
-            on={{
-                     change: evt => props.toggle(uid)
-            }}
-            checked={ completed ? 'checked' : null }
+        <li id={'task-' + uid} className='task'>
+            <input
+                type='checkbox' className='toggle'
+                id={'check-task-' + uid}
+                on={{
+                    change: evt => props.toggle(uid)
+                }}
+                checked={completed ? 'checked' : null}
             ></input>
-          { content }
-          <button
-            className='delete_button'
-            id={'del-task-'+props.uid}
-            on={{
-                click: evt => props.delete_(uid)
-            }}></button>
+            {content}
+            <button
+                className='delete_button'
+                id={'del-task-' + props.uid}
+                on={{
+                    click: evt => props.delete_(uid)
+                }}></button>
         </li>
     );
-/*
-    const checkboxAttrs = {
-        'type': 'checkbox',
-        'class': 'toggle',
-        'id': 'check-task-' + uid,
-        'on': {
-            change: evt => props.toggle(uid)
-        }
-    };
-
-    if (completed) {
-        checkboxAttrs['checked'] = 'checked';
-    }
-
-    const children = [
-        el('input', checkboxAttrs),
-        content,
-        el('button', {
-            'class': 'delete_button',
-            'id': 'del-task-' + props.uid,
-            'on': {
-                click: evt => props.delete_(uid)
-            }
-        })
-    ];
-
-    return el('li', {
-        'id': 'task-' + uid,
-        'class': 'task'
-    }, children);
-    */
 };
 
 export default TaskComponent;
